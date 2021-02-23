427cca1f16e7cf5859f456dc4a045091ff6e09eb
Sergey Filippov <sergey.filippov@outlook.com>
2021-02-23 18:33:14 +0100

    Project migrated to be complient with dotnet (core) build infrastructure

--- build.bat
+++ build.bat
@@ -1,14 +1,20 @@
 @echo off
 pushd %~dp0
-if "%WindowsSdkDir%" == "" call "%VS100COMNTOOLS%\vsvars32.bat"
+rem if "%WindowsSdkDir%" == "" call "%VS100COMNTOOLS%\vsvars32.bat"
 
 if "%BUILD_CONFIG%" == "" set BUILD_CONFIG=Debug
 
-echo Building %BUILD_CONFIG%...
-msbuild.exe ConsoleAppLauncher.sln /t:Rebuild /p:Configuration=%BUILD_CONFIG%
+
+rem msbuild.exe ConsoleAppLauncher.sln /t:Rebuild /p:Configuration=%BUILD_CONFIG%
 
 echo Running tests...
-packages\NUnit.Runners.2.6.2\tools\nunit-console.exe "ConsoleAppLauncher.Tests\bin\%BUILD_CONFIG%\ConsoleAppLauncher.Tests.dll" /framework=net-4.0 /labels
+dotnet test -c %BUILD_CONFIG% ConsoleAppLauncher.Tests/ConsoleAppLauncher.Tests.csproj
+
+echo Building %BUILD_CONFIG%...
+dotnet build -c %BUILD_CONFIG% ConsoleAppLauncher/ConsoleAppLauncher.csproj
+
+
+rem packages\NUnit.Runners.2.6.2\tools\nunit-console.exe "ConsoleAppLauncher.Tests\bin\%BUILD_CONFIG%\ConsoleAppLauncher.Tests.dll" /framework=net-4.0 /labels
 
 echo Done.
 popd
